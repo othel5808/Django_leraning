0c03908bd8b3ee50384221c8a3c3394e391721c4
Home_WindowOthel5808 <othel5808@gmail.com>
2021-11-21 13:35:17 +0900

    -somthing wrong...not adapted a CSS file in webpage

--- pinterestClone/settings.py
+++ pinterestClone/settings.py
@@ -119,8 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'statifiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+STATICFILES_DIR = [
+    BASE_DIR / "static"
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
