1ddf5f527e1494ada35a7095820383e21cae075e
Home_WindowOthel5808 <othel5808@gmail.com>
2021-11-21 21:31:50 +0900

    - inplementation shwoing Database item - fix: auto insert Data when refreshing page

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
@@ -14,8 +16,9 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world })
-
+        hello_world_list = HelloWorld.objects.all()
+        # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list })
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'get method!!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list })
