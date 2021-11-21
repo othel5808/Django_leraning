965d0d46939987ccbe78cfa57ea63f6f0c7f5c9a
Home_WindowOthel5808 <othel5808@gmail.com>
2021-11-21 20:37:35 +0900

    - testing post&get method

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == "POST" or request.method == 'post':
+        # return render(request, 'accountapp/hello_world.html', context={'text': 'POST method!!'})
+        return render(request, 'accountapp/hello_world.html', context={'text': 'post method!!'})
+
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'get method!!'})
