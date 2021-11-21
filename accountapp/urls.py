11ad2dc12ebb4b1816ba41de509151b9b71169db
Home_WindowOthel5808 <othel5808@gmail.com>
2021-11-21 22:45:52 +0900

    chapter 20 - adding Account create.html page. - adding crateview useing Classbased view

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp"
 
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+    # as_view() 를 적어야, 함수형 view 처럼 동작
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -22,3 +24,14 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list })
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    # form_calss = UserCreationForm
+    # reverse는 클래스 베이스 뷰에서 사용이 불가능, 따라서 _lazy는 클래스 대응이라고 생각하면 됨.
+    success_url = reverse_lazy('accountapp:hello_world')
+    # 어느 HTML을 통해서 봐야할지
+    # template_name='accountapp/create.html'
+    template_name='accountapp/create.html'
