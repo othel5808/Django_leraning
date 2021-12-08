96509afa495bad3500a229ef29c0ed00e1b3344d
Home_WindowOthel5808 <othel5808@gmail.com>
2021-12-08 21:29:56 +0900

    chapter 26 - added delete page and views

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = "accountapp"
 
@@ -14,5 +14,6 @@ urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='create'),
     # detail 페이지 구성 : 단, 특정 계정에 대한 detail을 보아야하기 때문에, primarykey를 받아야한다.
     path('detail/<int:pk>', AccountDetailView.as_view(), name="detail"),
-    path('update/<int:pk>', AccountUpdateView.as_view(), name="update")
+    path('update/<int:pk>', AccountUpdateView.as_view(), name="update"),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name="delete")
 ]
