f8eb8e6ae2e210622af462d5db3d823abbeea0a9
Home_WindowOthel5808 <othel5808@gmail.com>
2021-12-08 23:34:52 +0900

    chapter28 . added auth required with dacorator

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LogoutView
 from django.http import HttpResponse, HttpResponseRedirect
@@ -5,33 +6,34 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 # Create your views here.
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
+has_ownership = [
+    account_ownership_required, login_required
+]
 
+@login_required
 def hello_world(request):
-    if request.user.is_authenticated:
-        if request.method == "POST" or request.method == 'post':
-            # return render(request, 'accountapp/hello_world.html', context={'text': 'POST method!!'})
-            temp = request.POST.get('hello_world_input')
+    if request.method == "POST" or request.method == 'post':
+        # return render(request, 'accountapp/hello_world.html', context={'text': 'POST method!!'})
+        temp = request.POST.get('hello_world_input')
 
-            new_hello_world = HelloWorld()
-            new_hello_world.text = temp
-            new_hello_world.save()
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
 
-            hello_world_list = HelloWorld.objects.all()
-            # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list })
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        else:
-            hello_world_list = HelloWorld.objects.all()
-            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list })
+        hello_world_list = HelloWorld.objects.all()
+        # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list })
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list })
 
-class AccountLogoutView(LogoutView):
-    success_url = reverse_lazy('accountapp:hello_world')
 
 class AccountCreateView(CreateView):
     model = User
@@ -44,15 +46,17 @@ class AccountCreateView(CreateView):
     template_name = 'accountapp/create.html'
 
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDetailView(DetailView):
     model = User
     # 템플릿에서 사용하는 유저 객체의 이름을 설정 할 수 있음
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
-    def get(self, *args, **kwargs):
-        data = self.get_object()
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
@@ -60,6 +64,9 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
